97632b1188f686e507eb8a667b771dd867efefb8
DiepVanDinhUTC <168005351+DiepVanDinhUTC@users.noreply.github.com>
2026-09-22 08:33:34 +0700

    fix: resolve react version warning in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,11 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ]);
